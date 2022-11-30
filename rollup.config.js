5a1100e616a9081ab9f97748dcef1feb209784cb
chuande.zhong <chuande.zhong@zhenai.com>
2022-11-30 15:13:45 +0800

    chore: 新增打包文件格式和压缩能力

--- rollup.config.js
+++ rollup.config.js
@@ -2,22 +2,39 @@ import path from "path";
 import typescript from 'rollup-plugin-typescript2';
 import commonjs from "@rollup/plugin-commonjs";
 import babel from "@rollup/plugin-babel";
+import { terser } from 'rollup-plugin-terser'
 
-export default {
-  input: ["./src/index.ts"],
-  output: {
-    file: "./dist/bundle.js",
-    format: "umd",
-    name: "PowerMonitor" // 生成包的名字
+const rollupPlugins = [
+  commonjs(),
+  babel({
+    babelHelpers: 'runtime',
+    exclude: '**/node_modules/**',
+  }),
+  typescript({
+    tsconfig: path.resolve(__dirname, "./tsconfig.json")
+  }),
+]
+
+module.exports = [
+  {
+    input: ["./src/index.ts"],
+    output: {
+      file: "./dist/powerMonitor.min.js",
+      format: "umd",
+      name: "PowerMonitor", // 生成包的名字
+    },
+    plugins: [
+      ...rollupPlugins,
+      terser(),
+    ]
   },
-  plugins: [
-    commonjs(),
-    babel({
-      babelHelpers: 'runtime',
-      exclude: '**/node_modules/**',
-    }),
-    typescript({
-      tsconfig: path.resolve(__dirname, "./tsconfig.json")
-    }),
-  ],
-}
+  {
+    input: ["./src/index.ts"],
+    output: {
+      file: "./dist/powerMonitor.common.js",
+      format: "cjs",
+      name: "PowerMonitor", // 生成包的名字
+    },
+    plugins: rollupPlugins
+  }
+]
